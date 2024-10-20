0e0c49596d0bcdd9d935ea00ae09782280b492fe
Dylan <dylan.10293@gmail.com>
2024-10-20 15:34:02 -0400

    Basic layout made with colors for visualization

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top-bar">
+        top-bar goes here
+      </div>
+      <div className="main-container">
+        <div className="side-bar">
+          Sidebar goes here
+        </div>
+        <div className="content-container">
+          Content goes here
+        </div>
+      </div>
     </div>
   );
 }
